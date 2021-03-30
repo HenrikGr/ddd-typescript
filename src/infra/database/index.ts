c0c4127b3cb16d91df263f62c8d5bc9470facefd
Henrik Grönvall <hgc-ab@outlook.com>
2021-03-30 18:31:33 +0200

    Small fix to the dbClient

--- src/infra/database/index.ts
+++
@@ -1,9 +0,0 @@
-
-import { DbClient, DbClientConfiguration } from '@hgc-sdk/mongo-db'
-
-const connectionOpts = DbClientConfiguration.create()
-const dbClient = DbClient.create(connectionOpts)
-
-export {
-  dbClient
-}
